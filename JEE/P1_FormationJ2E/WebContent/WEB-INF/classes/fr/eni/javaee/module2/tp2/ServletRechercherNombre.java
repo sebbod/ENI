2fa9eafc74e2b3fe0471edd40106b65c997f6599
sebbod <bod456@gmail.com>
2021-11-09 17:43:31 +0100

    tp2 ok

--- JEE/P1_FormationJ2E/WebContent/WEB-INF/classes/fr/eni/javaee/module2/tp2/ServletRechercherNombre.java
+++ JEE/P1_FormationJ2E/WebContent/WEB-INF/classes/fr/eni/javaee/module2/tp2/ServletRechercherNombre.java
@@ -15,87 +15,62 @@ import org.apache.commons.lang3.StringUtils;
 /**
  * Servlet implementation class ServletRechercherNombre
  */
-@WebServlet(
-		urlPatterns="/modules/module2/tp2/ServletRechercherNombre",
-		initParams= {
-						@WebInitParam(name="BORNE_MIN",value="10"),
-						@WebInitParam(name="BORNE_MAX",value="15")
-					}
-		)
+@WebServlet(urlPatterns = "/modules/module2/tp2/ServletRechercherNombre", initParams = {
+		@WebInitParam(name = "BORNE_MIN", value = "10"), @WebInitParam(name = "BORNE_MAX", value = "20") })
 public class ServletRechercherNombre extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+
 	private int nombreATrouver;
 	private boolean win;
-	private int borneMin=0;
-	private int borneMax=10;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletRechercherNombre() {
-        super();
-        //Impossibilité de lire le fichier web.xml
-    }
-    
-    @Override
-    public void init() throws ServletException {
-    	if(this.getInitParameter("BORNE_MIN")!=null &&
-    			!this.getInitParameter("BORNE_MIN").equals(""))
-    	{
-    		String valeurLue=this.getInitParameter("BORNE_MIN");
-			if(StringUtils.isNumeric(valeurLue))
-			{
-				this.borneMin=Integer.parseInt(valeurLue);
-			}
-    	}
-    	
-    	if(this.getInitParameter("BORNE_MAX")!=null &&
-    			!this.getInitParameter("BORNE_MAX").equals(""))
-    	{
-    		String valeurLue=this.getInitParameter("BORNE_MAX");
-			if(StringUtils.isNumeric(valeurLue))
-			{
-				this.borneMax=Integer.parseInt(valeurLue);
-			}
-    	}
-			
-    	//On recherche entre 0 inclus et la différence max-min+1 (car valeur max exclue)
-    	//On ajoute ensuite borneMin pour arriver dans la plage ([borneMin;borneMax])
-    	this.nombreATrouver=new Random().nextInt(this.borneMax-this.borneMin+1)+this.borneMin;
-    	this.win=false;
-    	System.out.println(this.nombreATrouver);
-    }
+	private int borneMin = 0;
+	private int borneMax = 10;
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//Récupérer la valeur
-		String nombreSaisie = request.getParameter("nombre");
-		if(nombreSaisie.equals(String.valueOf(this.nombreATrouver)) && !this.win)
-		{
-			response.sendRedirect("succes.html");
-			this.win=true;
+	public ServletRechercherNombre() {
+		super();
+		// Impossibilité de lire le fichier web.xml
+	}
+
+	@Override
+	public void init() throws ServletException {
+		if (this.getInitParameter("BORNE_MIN") != null && !this.getInitParameter("BORNE_MIN").equals("")) {
+			String valeurLue = this.getInitParameter("BORNE_MIN");
+			if (StringUtils.isNumeric(valeurLue)) {
+				this.borneMin = Integer.parseInt(valeurLue);
+			}
 		}
-		else
-		{
+
+		if (this.getInitParameter("BORNE_MAX") != null && !this.getInitParameter("BORNE_MAX").equals("")) {
+			String valeurLue = this.getInitParameter("BORNE_MAX");
+			if (StringUtils.isNumeric(valeurLue)) {
+				this.borneMax = Integer.parseInt(valeurLue);
+			}
+		}
+
+		// On recherche entre 0 inclus et la différence max-min+1 (car valeur max
+		// exclue)
+		// On ajoute ensuite borneMin pour arriver dans la plage ([borneMin;borneMax])
+		this.nombreATrouver = new Random().nextInt(this.borneMax - this.borneMin + 1) + this.borneMin;
+		this.win = false;
+		System.out.println(this.nombreATrouver);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Récupérer la valeur
+		String nombreSaisie = request.getParameter("nombre");
+		if (nombreSaisie.equals(String.valueOf(this.nombreATrouver)) && !this.win) {
+			response.sendRedirect("succes.html");
+			this.win = true;
+		} else {
 			response.sendRedirect("echec.html");
 		}
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
